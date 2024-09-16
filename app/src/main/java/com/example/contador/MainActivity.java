2250fbab4354ea34fc9513c98c6f2e36a8ab4511
Carlos <carlos.s2006@aluno.ifsc.edu.br>
2024-09-16 10:33:44 -0300

    IMC

--- app/src/main/java/com/example/contador/MainActivity.java
+++ app/src/main/java/com/example/contador/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -15,25 +16,63 @@ public class MainActivity extends AppCompatActivity {
     Button button;
     TextView textView;
 
+    EditText peso;
+    EditText altura;
+
+    Float resultado = null;
     Random Contador = new Random(20);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = findViewById(R.id.textView);
+       peso = findViewById(R.id.Peso);
+       altura = findViewById(R.id.Altura);
+
+        textView = findViewById(R.id.Resultado);
         button =findViewById(R.id.BtnClicke);
 
-        button.setText("click");
+
+
+         button.setText("Calcular");
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                float p;
+                p = Float.parseFloat(peso.getText().toString());
+                float a;
+
+                a = Float.valueOf(altura.getText().toString());
+
+                resultado = p/a;
+
+
                 Button b = (Button) view;
 
+                if (resultado <18.5){
+                    textView.setText(" Abaixo do peso: " + resultado );
 
-                textView.setText("Sorteio " + Contador.nextInt(30));
+                }
+                else if (resultado > 18.5 && resultado <24.9){
 
+                    textView.setText("Peso normal:" + resultado);
+
+                } else if (resultado > 25 && resultado < 29.9) {
+                    textView.setText("Sobre Peso:" + resultado);
+                    
+                    
+                } else if (resultado >30 && resultado<34.9) {
+                    textView.setText("Obesidade 1:" + resultado);
+                    
+                } else if (resultado >35 && resultado<39.9) {
+                    textView.setText("Obesidade 2:" + resultado);
+
+                    
+                }
+                else {
+                    textView.setText("Obesidade 3:" + resultado);
+                }
             }
         });
     }
