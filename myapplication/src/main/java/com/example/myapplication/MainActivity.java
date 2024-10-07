97c953aab58d06f9ce3aeffd9b8ee41bbae9d1b3
carlos <soaresecarlos15@gmail.com>
2024-10-07 10:13:51 -0300

    Listagem

--- myapplication/src/main/java/com/example/myapplication/MainActivity.java
+++ myapplication/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
 
 public class MainActivity extends AppCompatActivity {
-
-    @Override
+    String[] nomes = new String[]{"datena","ciro","caderada","eleição"};
+    ListView list;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        list = findViewById(R.id.list2);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,android.R.id.text1,nomes);
+    list.setAdapter(arrayAdapter);
     }
 }
