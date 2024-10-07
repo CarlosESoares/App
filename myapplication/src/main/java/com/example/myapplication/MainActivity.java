b5e5b566840868848ef8e3187bba513b1a700139
carlos <soaresecarlos15@gmail.com>
2024-10-07 10:59:24 -0300

    Listagem final

--- myapplication/src/main/java/com/example/myapplication/MainActivity.java
+++ myapplication/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.lang.reflect.Array;
 
@@ -17,5 +20,19 @@ public class MainActivity extends AppCompatActivity {
         list = findViewById(R.id.list2);
         ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,android.R.id.text1,nomes);
     list.setAdapter(arrayAdapter);
+    list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            Toast.makeText(getApplicationContext(),Integer.toString(i)+
+                    " "+
+                    nomes[i],Toast.LENGTH_LONG).show();
+        }
+    });
+{
+
+
+        }
     }
+
 }
