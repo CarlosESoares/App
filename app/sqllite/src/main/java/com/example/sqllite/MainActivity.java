e387b2c21e9a878bfd0c4f075de506c78868ef70
carlos <Carlos.s2006@aluno.ifsc.edu.br>
2024-10-21 11:22:01 -0300

    Carregar listagem

--- app/sqllite/src/main/java/com/example/sqllite/MainActivity.java
+++ app/sqllite/src/main/java/com/example/sqllite/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.sqllite;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     EditText editTextnome,editTextemail,editTextdata;
 
     Button button;
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         editTextdata = findViewById(R.id.EditTextData);
         editTextemail = findViewById(R.id.EditTextEmail);
         editTextnome = findViewById(R.id.EditTextNome);
+        listView= findViewById(R.id.List);
         button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,12 +50,31 @@ public class MainActivity extends AppCompatActivity {
                 }else {
                     Toast.makeText(getApplicationContext(),"Erro na inserção",Toast.LENGTH_LONG).show();
                 }
+                carregarListagem();
+
             }
+
         });
 
 
         database = openOrCreateDatabase("meudb", MODE_PRIVATE,null);
         database.execSQL("CREATE TABLE IF NOT exists pessoas (id INTEGER primary kEY AUTOINCREMENT,nome varchar, email varchar,dtnsc DATE)");
 
+    }
+    public void carregarListagem(){
+        Cursor cursor = database.rawQuery( "Select * from pessoas",null );
+        cursor.moveToFirst();
+        ArrayList<String> nomes = new ArrayList<>();
+
+        while (!cursor.isAfterLast()){
+            nomes.add(cursor.getString(1));
+            cursor.moveToNext();
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                nomes);
+
+    listView.setAdapter(adapter);
     }
 }
